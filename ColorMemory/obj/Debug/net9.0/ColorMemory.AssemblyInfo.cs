b77ad72aa4dda298b15c772954c3c589b740d0d3
carmen <luboy@naver.com>
2025-03-09 20:16:14 +0900

    rebuild the solution

--- ColorMemory/obj/Debug/net9.0/ColorMemory.AssemblyInfo.cs
+++ ColorMemory/obj/Debug/net9.0/ColorMemory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColorMemory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502bc30a4400f170b12a6fd86dd38f2c3165d175")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a089d6439c30b6ce8db11963af2105a89cc7ecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColorMemory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColorMemory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
