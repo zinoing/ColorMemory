a2ed590b9b575e33ee7151ca014f1b8f0000b0e0
carmen <luboy@naver.com>
2025-03-14 23:52:23 +0900

    delete dockerfile

--- ColorMemory/Program.cs
+++ ColorMemory/Program.cs
@@ -1,10 +1,11 @@
+using ColorMemory.DBContext;
 using ColorMemory.Repository;
 using Microsoft.AspNetCore.Http.Timeouts;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 서비스 등록 전에 Host 옵션 설정
 builder.Host.ConfigureHostOptions(options =>
 {
     options.ShutdownTimeout = TimeSpan.FromSeconds(60);
@@ -17,6 +18,9 @@ builder.Services.AddScoped<NationalRankingDb>();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<GameDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
--- ColorMemory/obj/Debug/net9.0/ColorMemory.AssemblyInfo.cs
+++ ColorMemory/obj/Debug/net9.0/ColorMemory.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColorMemory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c52475820c6fdfd585b39deb656692906d3d93ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adaa51e82e4c72a3f645f74682c477bb6b5348af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColorMemory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColorMemory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
