b08f1ca0d53046b2d0f3dadfa56bc1442cfef34e
carmen <luboy@naver.com>
2025-03-15 14:29:34 +0900

    add migrations

---
+++ ColorMemory/Migrations/20250314162831_InitialCreate.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ColorMemory.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Artworks",
+                columns: table => new
+                {
+                    ArtworkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Artist = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artworks", x => x.ArtworkId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    PlayerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Score = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.PlayerId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PlayerArtCollections",
+                columns: table => new
+                {
+                    ArtworksArtworkId = table.Column<int>(type: "int", nullable: false),
+                    PlayersPlayerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerArtCollections", x => new { x.ArtworksArtworkId, x.PlayersPlayerId });
+                    table.ForeignKey(
+                        name: "FK_PlayerArtCollections_Artworks_ArtworksArtworkId",
+                        column: x => x.ArtworksArtworkId,
+                        principalTable: "Artworks",
+                        principalColumn: "ArtworkId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlayerArtCollections_Players_PlayersPlayerId",
+                        column: x => x.PlayersPlayerId,
+                        principalTable: "Players",
+                        principalColumn: "PlayerId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerArtCollections_PlayersPlayerId",
+                table: "PlayerArtCollections",
+                column: "PlayersPlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlayerArtCollections");
+
+            migrationBuilder.DropTable(
+                name: "Artworks");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
