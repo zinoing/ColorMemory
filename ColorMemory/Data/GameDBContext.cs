5df255abfd9caac2c0ae328041343bf9b7e86eb6
carmen <luboy@naver.com>
2025-03-15 14:28:38 +0900

    add GameDBContext

---
+++ ColorMemory/Data/GameDBContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ColorMemory.Data
+{
+    public class Player
+    {
+        [Key]
+        public int PlayerId { get; set; }
+        public int Score { get; set; }
+
+        public ICollection<PlayerArtwork> PlayerArtworks { get; set; }
+    }
+
+    public class Artwork
+    {
+        public int ArtworkId { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+    }
+
+    public class PlayerArtwork
+    {
+        public int PlayerId { get; set; }
+        public Player Player { get; set; }
+
+        public int ArtworkId { get; set; }
+        public Artwork Artwork { get; set; }
+
+        public bool HasIt { get; set; } = false;
+    }
+
+
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(DbContextOptions<GameDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Artwork> Artworks { get; set; }
+        public DbSet<PlayerArtwork> PlayerArtworks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PlayerArtwork>()
+                .HasKey(pa => new { pa.PlayerId, pa.ArtworkId });
+
+            modelBuilder.Entity<PlayerArtwork>()
+                .HasOne(pa => pa.Player)
+                .WithMany(p => p.PlayerArtworks)
+                .HasForeignKey(pa => pa.PlayerId);
+
+            modelBuilder.Entity<PlayerArtwork>()
+                .HasOne(pa => pa.Artwork)
+                .WithMany()
+                .HasForeignKey(pa => pa.ArtworkId);
+        }
+
+    }
+}
