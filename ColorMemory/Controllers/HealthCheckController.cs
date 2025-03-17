4d0702781855d4aea34798ce9a2a0be5782c27fe
carmen <luboy@naver.com>
2025-03-17 23:54:04 +0900

    rebuild the project

--- ColorMemory/Controllers/HealthCheckController.cs
+++ ColorMemory/Controllers/HealthCheckController.cs
@@ -18,6 +18,7 @@ namespace ColorMemory.Controllers
         [HttpGet]
         public IActionResult GetHealth()
         {
+            _logger.LogInformation("Health check endpoint was hit.");
             return StatusCode(200);
         }
     }
