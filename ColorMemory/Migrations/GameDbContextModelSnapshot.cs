915afa46ad65c86a850f31a221707338ad6ef8cc
carmen <luboy@naver.com>
2025-03-17 19:25:23 +0900

    rebuild the project

--- ColorMemory/Migrations/GameDbContextModelSnapshot.cs
+++ ColorMemory/Migrations/GameDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace ColorMemory.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.13")
+                .HasAnnotation("ProductVersion", "8.0.40")
                 .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             MySqlModelBuilderExtensions.AutoIncrementColumns(modelBuilder);
